705d8506d9852146ad093cb514dbd75dcc4b9dd9
TomasTatra <t.vikiszaly@gmail.com>
2025-03-29 16:00:41 +0100

    basic animations

--- Assets/Scripts/MainGameEngine.cs
+++ Assets/Scripts/MainGameEngine.cs
@@ -3,16 +3,17 @@ using UnityEngine;
 public class MainGameEngine : MonoBehaviour
 {
 
-    private InputMapping playerInput;
-    private float moveInput;
-    private Camera myCamera;
-    public int[] widthOfGame = new int[2];
+    InputMapping playerInput;
+    float moveInput;
+    double evolution = 0;
+    int actualStep = 0;
+    [SerializeField] GameObject[] animators;
+    [SerializeField] int[] widthOfGame = new int[2];
+    [SerializeField] double[] evolutionSteps = new double[5];
     [SerializeField] float speed = 0.2f;
 
-    //InputAction leftRight;
     private void Awake()
     {
-        myCamera = Camera.main;
         playerInput = new InputMapping();
         playerInput.Player.Scroll.performed += ctx => moveInput = ctx.ReadValue<float>();
         playerInput.Player.Scroll.canceled += ctx => moveInput = 0;
@@ -21,15 +22,10 @@ public class MainGameEngine : MonoBehaviour
     private void OnEnable() => playerInput.Enable();
     private void OnDisable() => playerInput.Disable();
 
-    void Start()
-    {
-        //leftRight = InputSystem.actions.FindAction("LeftRight");
-    }
+    void Start() {}
 
     void Update()
     {
-        //int go = leftRight.ReadValue<int>();
-        //camera.transform.position -= new Vector3(moveInput, 0, 0);
         if (moveInput != 0)
             MoveCamera(moveInput);
     }
@@ -37,14 +33,26 @@ public class MainGameEngine : MonoBehaviour
     void MoveCamera(float input)
     {
 
-        if ((input < 0 && myCamera.transform.position.x > widthOfGame[0]) ||
-            (input > 0 && myCamera.transform.position.x < widthOfGame[1]))
+        if ((input < 0 && transform.position.x > widthOfGame[0]) ||
+            (input > 0 && transform.position.x < widthOfGame[1]))
         {
-            myCamera.transform.position += speed * new Vector3(moveInput, 0, 0);
+            transform.position += speed * new Vector3(moveInput, 0, 0);
+            Evolve(2);
         }
     }
 
-    //double EaseInOutCubic(double x) {
-    //    return x< 0.5 ? 4 * x* x* x : 1 - Math.Pow(-2 * x + 2, 3) / 2;
-    //}
+    void Evolve(double by)
+    {
+        evolution += by;
+        if (actualStep >= evolutionSteps.Length)
+            return;
+        if (evolution > evolutionSteps[actualStep])
+        {
+            actualStep++;
+            foreach (var animator in animators)
+            {
+                animator.GetComponent<Animator>().SetInteger("stage", actualStep);
+            }
+        }
+    }
 }
